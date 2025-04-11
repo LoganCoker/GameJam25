60ddb894399207a359442ad0c17d5204a4ebcc8a
tax evasion <keahi.temple@gmail.com>
2025-04-11 18:58:37 -0500

    basic movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Ground Movement
+    private Rigidbody rb;
+    public float MoveSpeed = 5f;
+    private float moveHorizontal;
+    private float moveForward;
+
+    // Jumping movement
+    public float jumpForce = 10f;
+    // Multiplies gravity when falling
+    public float fallMultiplier = 2.5f; 
+    // Multiplies gravity when ascending
+    public float ascendMultiplier = 2f; 
+    private bool isGrounded = true;
+    public LayerMask groundLayer;
+    private float groundCheckTimer = 0f;
+    private float groundCheckDelay = 0.3f;
+    private float playerHeight;
+    private float raycastDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the rigidbody component and freeze its rotation/
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        // set the raycast to be slightly beneath the player's feet
+        playerHeight = GetComponent<CapsuleCollider>().height * transform.localScale.y;
+        raycastDistance = (playerHeight / 2) + 0.2f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveForward = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+        // Checking when we're on the ground and keeping track of our ground check delay
+        if (!isGrounded && groundCheckTimer <= 0f)
+        {
+            Vector3 rayOrigin = transform.position + Vector3.up * 0.1f;
+            isGrounded = Physics.Raycast(rayOrigin, Vector3.down, raycastDistance, groundLayer);
+        }
+        else
+        {
+            groundCheckTimer -= Time.deltaTime;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+        ApplyJumpPhysics();
+    }
+
+    void MovePlayer()
+    {
+
+        Vector3 movement = (transform.right * moveHorizontal + transform.forward * moveForward).normalized;
+        Vector3 targetVelocity = movement * MoveSpeed;
+
+        // Apply movement to the Rigidbody
+        Vector3 velocity = rb.velocity;
+        velocity.x = targetVelocity.x;
+        velocity.z = targetVelocity.z;
+        rb.velocity = velocity;
+
+        // If we aren't moving and are on the ground, stop velocity so we don't slide
+        if (isGrounded && moveHorizontal == 0 && moveForward == 0)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
+    void Jump()
+    {
+        isGrounded = false;
+        groundCheckTimer = groundCheckDelay;
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+
+    void ApplyJumpPhysics()
+    {
+        // Falling
+        if (rb.velocity.y < 0) 
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.fixedDeltaTime;
+        } // Rising
+        else if (rb.velocity.y > 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * ascendMultiplier  * Time.fixedDeltaTime;
+        }
+    }
+}
