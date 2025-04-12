3daba22648dd67b6086d7bb298aab6706a3fd96d
Logan <loganc47sac@gmail.com>
2025-04-12 18:34:17 -0500

    moving platforms, first animations

--- Assets/Scripts/EnviornmentScripts/PlatformSide.cs
+++ Assets/Scripts/EnviornmentScripts/PlatformSide.cs
@@ -4,15 +4,102 @@ using UnityEngine;
 
 public class PlatformSide : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    #region publics
+    public float dis;
+    public float speed;
+    public bool MoveX;
+    #endregion
+
+    #region privates
+    private bool isMoving;
+    private Vector3 start;
+    private float dest;
+    #endregion
+
+    void Start() {
+        start = transform.position;
+        if (MoveX) {
+            dest = start.x + dis;
+        } else { 
+            dest = start.z + dis;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (!isMoving) {
+            if (MoveX) {
+                if (transform.position.x <= start.x) {
+                    StartCoroutine(MovePosX());
+                } else {
+                    StartCoroutine(MoveNegX());
+                }
+            } else {
+                if (transform.position.z <= start.z) {
+                    StartCoroutine(MovePosZ());
+                } else {
+                    StartCoroutine(MoveNegZ());
+                }
+            }
+        }
+    }
+
+    IEnumerator MovePosX() {
+        isMoving = true;
+        float timing = 0f;
+        while (timing < speed && transform.position.x <= dest) {
+            timing += Time.deltaTime;
+            transform.position += Vector3.right * 1 / (speed * 20);
+            yield return null;
+        }
+        yield return new WaitForSeconds(speed);
+        isMoving = false;
+    }
+
+    IEnumerator MoveNegX() {
+        isMoving = true;
+        float timing = 0f;
+        while (timing < speed && transform.position.x > start.x) {
+            timing += Time.deltaTime;
+            transform.position += Vector3.left * 1 / (speed * 20);
+            yield return null;
+        }
+        yield return new WaitForSeconds(speed);
+        isMoving = false;
+    }
+    
+    IEnumerator MovePosZ() {
+        isMoving = true;
+        float timing = 0f;
+        while (timing < speed && transform.position.z <= dest) {
+            timing += Time.deltaTime;
+            transform.position += Vector3.forward * 1 / (speed * 20);
+            yield return null;
+        }
+        yield return new WaitForSeconds(speed);
+        isMoving = false;
+    }
+
+    IEnumerator MoveNegZ() {
+        isMoving = true;
+        float timing = 0f;
+        while (timing < speed && transform.position.z > start.z) {
+            timing += Time.deltaTime;
+            transform.position += Vector3.back * 1 / (speed * 20);
+            yield return null;
+        }
+        yield return new WaitForSeconds(speed);
+        isMoving = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            other.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            other.transform.SetParent(null);
+        }
     }
 }
--- Assets/Scripts/EnviornmentScripts/PlatformUp.cs
+++ Assets/Scripts/EnviornmentScripts/PlatformUp.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlatformUp : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlatformUp : MonoBehaviour {
+
+    #region publics
+    public float dis;
+    public float speed;
+    #endregion
+
+    #region privates
+    private bool isMoving;
+    private Vector3 start;
+    private float dest;
+    #endregion
+
+    void Start() {
+        start = transform.position;
+        dest = start.y + dis;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (!isMoving) {
+            if (transform.position.y <= start.y) {
+                StartCoroutine(MoveUp());
+            } else {
+                StartCoroutine(MoveDown()); 
+            }
+        }
+    }
+
+    IEnumerator MoveUp() { 
+        isMoving = true;
+        float timing = 0f;
+        while (timing < speed && transform.position.y <= dest) {
+            timing += Time.deltaTime;
+            transform.position += Vector3.up * 1/(speed * 20);
+            yield return null; 
+        }
+        yield return new WaitForSeconds(speed);
+        isMoving = false;
+    }
+
+    IEnumerator MoveDown() {
+        isMoving = true;
+        float timing = 0f;
+        while (timing < speed && transform.position.y > start.y) {
+            timing += Time.deltaTime;
+            transform.position += Vector3.down * 1 / (speed * 20);
+            yield return null;
+        }
+        yield return new WaitForSeconds(speed);
+        isMoving = false;
     }
 }
+ 
