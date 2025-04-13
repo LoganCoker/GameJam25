9f3868abbc9f90f67799763034c056dfcbb80031
gabethomp <gabealan9@gmail.com>
2025-04-13 09:55:29 -0500

    MR boss arena made

--- Assets/Scripts/EnviornmentScripts/SpawnMR.cs
+++ Assets/Scripts/EnviornmentScripts/SpawnMR.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class SpawnMR : MonoBehaviour {
     public BossAI BossAI;
     public MageAI MageAI;
+    public GameObject EntranceBarrier, ExitBarrier;
 
     void Awake() {
         BossAI.enabled = false;
         MageAI.enabled = false;
+        EntranceBarrier.SetActive(false);
+        ExitBarrier.SetActive(false);
     }
     void OnTriggerEnter(Collider Obj) {
         if (Obj.CompareTag("Player")) { 
             BossAI.enabled = true;
             MageAI.enabled = true;
+            EntranceBarrier.SetActive(true);
+            ExitBarrier.SetActive(true);
         }
     }
 }
