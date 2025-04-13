7c153564b28b4b19dbabb0fb62e893e026aec232
tax evasion <keahi.temple@gmail.com>
2025-04-13 16:35:02 -0500

    dfdsfdsfs

--- Assets/Scripts/EnviornmentScripts/SpawnGOD.cs
+++ Assets/Scripts/EnviornmentScripts/SpawnGOD.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class SpawnGOD : MonoBehaviour {
     public BossAI BossAI;
+    public BackgroundMusic backgroundMusic;
     void Awake() {
         BossAI.enabled = false;
     }
     void OnTriggerEnter(Collider Obj) {
-        if (Obj.CompareTag("Player")) { BossAI.enabled = true; }
+        if (Obj.CompareTag("Player")) { BossAI.enabled = true; backgroundMusic.isBossfightStarted = true; }
     }
 }
