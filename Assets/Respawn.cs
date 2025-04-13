1875dd9ae244cf92ba1fc0fbe5472890521d2d34
Logan <loganc47sac@gmail.com>
2025-04-13 02:16:38 -0500

    start to movement level

---
+++ Assets/Respawn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            other.gameObject.transform.position = transform.position;
+        }
+    }
+}
